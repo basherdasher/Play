f3996ab5566fbddf3a78b7e81cd953b46a6ee925
basherdasher <bogachevadasha1997@mail.ru>
2022-12-26 21:15:42 +0300

    добавлен второй сервис

---
+++ Play.Inventory/src/Play.Inventory.Service/Dtos.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Play.Inventory.Service.Dtos
+{
+    public record GrantItemDto(Guid UserId, Guid CatalogItemId, int Quantity);
+
+    public record InventoryItemDto(Guid CatalogItemId, int Quantity, DateTimeOffset AcquiredDate);
+}
---
+++ Play.Inventory/src/Play.Inventory.Service/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using Play.Inventory.Service.Dtos;
+using Play.Inventory.Service.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Play.Inventory.Service
+{
+    public static class Extensions
+    {
+        public static InventoryItemDto AsDto(this InventoryItem item)
+        {
+            return new InventoryItemDto(
+                item.CatalogItemId,
+                item.Quantity,
+                item.AcquiredDate
+                );
+        }
+    }
+}
